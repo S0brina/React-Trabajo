3faf51493edd86594e8461db11cf3bde2937f263
S0brina <118025166+S0brina@users.noreply.github.com>
2023-01-28 16:36:26 -0500

    Add files via upload

---
+++ src/Presentation/LoginC/Login_Cliente.jsx
@@ -0,0 +1,46 @@
+import { useNavigate } from "react-router-dom"
+import LoginForm from "./components/LoginForm"
+
+function LoginPage() {
+
+    const navigate = useNavigate()
+
+    const onLoginOk = function(
+        usuario, password
+    ) {
+        if (usuario === "pw" 
+            && password === "123") {
+
+            const dataUsuario = {
+                username : usuario,
+                password : password
+            }
+
+            // JSON.stringify : convierte objetos js a JSON (string)
+            const dataUsuarioJSON = JSON.stringify(dataUsuario)
+            console.log(dataUsuario)
+            console.log(dataUsuarioJSON)
+            // Guardado en session storage
+            sessionStorage.setItem("DATA_USUARIO", dataUsuarioJSON)
+
+
+            navigate("/main", {
+                state : {
+                    username : usuario
+                }
+            })
+        }
+    }
+
+    return <div className="login">
+        <div className="login-screen">
+            <div className="app-title">
+                <h1>Inicio de Sesión</h1>
+            </div>
+            <LoginForm 
+            onLoginOk={ onLoginOk } />
+        </div>
+    </div>
+}
+
+export default LoginPage
---
+++ src/Presentation/LoginC/components/LoginForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+function LoginForm(props) {
+    const [usuario, setUsuario] = useState("")
+    const [password, setPassword] = useState("")
+
+    const butOnClick = function() {
+        console.log("USuario:", usuario)
+        console.log("Password:", password)
+        props.onLoginOk(usuario, password)
+    }
+
+    return <form>
+        <div className="formulario">
+            <div className="control-group">
+                <input type="text" id="login-name" placeholder="Nombre de usuario">
+                value={ usuario }
+                onChange={ function(evt) { setUsuario(evt.target.value) } }</input>
+            </div>
+            <div className="control-group">
+                <input type="password" id="login-pass" placeholder="Contraseña">
+                value={ password }
+                onChange={ function(evt) { setPassword(evt.target.value) } }</input>
+            </div>
+        </div>
+        
+        <button className="btn" type="button"
+            onClick={ butOnClick }>
+            Login
+        </button>
+        <a className="password-link" href="#">¿Olvidaste tu contraseña?</a>
+    </form>
+}
+
+export default LoginForm
