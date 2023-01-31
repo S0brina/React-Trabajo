ceb2146f6b62cd9678d674964324b19a178f578b
L34305 <L34305@lab.aca>
2023-01-30 19:46:21 -0500

    falta header

---
+++ src/Presentation/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return <div className = "container" >
+        <Headers className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                
+            </a>
+            <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" className="nav-link px-2 link-secondary">Inicio</a></li>
+            </ul>
+            
+
+        </Headers>
+
+    </div>
+}
