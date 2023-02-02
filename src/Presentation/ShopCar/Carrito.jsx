406ff24b008d625224a2bc3dafaf2f2ba4b17e31
S0brina <solanosabrina78@gmail.com>
2023-02-02 10:27:06 -0500

    .

--- src/Presentation/ShopCar/Carrito.jsx
+++ src/Presentation/ShopCar/Carrito.jsx
@@ -4,25 +4,25 @@ import NavBar from "../NavbarUser"
 function Carrito() {
   const [cards] = useState([
     {
-        img: "",
-        title: "Card-1",
-        priceU: "40",
+        img: "https://th.bing.com/th/id/OIP.bV2Ekh5Np4SGQ9TrdamsMwHaF6?pid=ImgDet&rs=1",
+        title: "Coca Cola personal",
+        priceU: "4",
         cant: "1",
-        mtotal: "40",
+        mtotal: "4",
     },
     {
-        img: "",
-        title: "Card-1",
-        priceU: "20",
+        img: "https://plazavea.vteximg.com.br/arquivos/ids/1538987-220-220/26887.jpg?v=637548077932200000",
+        title: "Pizza Americana Familiar",
+        priceU: "50",
         cant: "1",
-        mtotal: "40",
+        mtotal: "50",
     },
     {
-        img: "",
-        title: "Card-1",
-        priceU: "46",
+        img: "https://plazavea.vteximg.com.br/arquivos/ids/415006-1000-1000/20200389.jpg?v=637370980289900000",
+        title: "Inca Kola Personal",
+        priceU: "4",
         cant: "1",
-        mtotal: "40",
+        mtotal: "4",
     },
     
   ]);
@@ -30,8 +30,8 @@ function Carrito() {
     return (
         <main>
             <NavBar />    
-            <div className="section-car bg-light">
-        <h1>Carrito de compras</h1>
+            <div className="section-car bg-light ">
+        <h1 className="text-dark">Carrito de compras</h1>
         <div>
           {cards.map((item) => {
             const { img, title, priceU, cant, mtotal } = item;
